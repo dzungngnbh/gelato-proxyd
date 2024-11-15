35972bbb195c994ef363909e686ce4db72692386
dzungngnbh <yearly.woven0j@icloud.com>
2024-11-16 00:18:27 +0700

    feat: Add fetching and upsert key from/to database and fix test.

--- auth.go
+++ auth.go
@@ -1,5 +1,13 @@
 package proxyd
 
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/jackc/pgx/v5"
+	"os"
+	"strings"
+)
+
 // AuthStore is a struct that holds the keys for the authentication paths in url
 // Example: http://endpoint?key=value
 type AuthStore struct {
@@ -7,8 +15,14 @@ type AuthStore struct {
 }
 
 func NewAuthStore() *AuthStore {
+	keys := FetchKeysFromDb()
+	if keys == nil {
+		keys = make(map[string]string)
+		log.Warn("No authentication keys found in database")
+	}
+
 	return &AuthStore{
-		keys: make(map[string]string),
+		keys: keys,
 	}
 }
 
@@ -26,11 +40,98 @@ func (a *AuthStore) Remove(key string) {
 	delete(a.keys, key)
 }
 
-// Valid checks if the key value pair is valid
-func (a *AuthStore) Valid(key string, value string) bool {
+// Valid returns value if key is valid
+func (a *AuthStore) Valid(key string, value string) string {
 	if key == "" || value == "" {
-		return false
+		return ""
 	}
 
-	return a.keys[key] == value
+	if a.keys[key] == value {
+		return value
+	}
+
+	return ""
+}
+
+// Get returns value if key is valid
+func (a *AuthStore) Get(key string) string {
+	if key == "" {
+		return ""
+	}
+
+	return a.keys[key]
+}
+
+// GetConn returns a connection to the database.
+func GetConn() *pgx.Conn {
+	pgUri := os.Getenv("DATABASE_URL")
+	if pgUri == "" {
+		log.Error("DATABASE_URL not set")
+		return nil
+	}
+
+	conn, err := pgx.Connect(context.Background(), pgUri)
+	if err != nil {
+		log.Error("unable to connect to database", "err", err)
+		return nil
+	}
+
+	return conn
+}
+
+// FetchKeysFromDb returns the keys from database.
+func FetchKeysFromDb() map[string]string {
+	conn := GetConn()
+	if conn == nil {
+		return nil
+	}
+	defer conn.Close(context.Background())
+
+	// query auth keys from database
+	var authKey string
+	var authValue string
+	var isDisabled bool
+
+	rows, err := conn.Query(context.Background(), "select auth_key, auth_value, is_disabled from auth_keys")
+	if err != nil {
+		log.Error("query database failed", "err", err)
+		return nil
+	}
+
+	res := make(map[string]string)
+	_, err = pgx.ForEachRow(rows, []any{&authKey, &authValue, &isDisabled}, func() error {
+		if !isDisabled {
+			res[authKey] = authValue
+		}
+		return nil
+	})
+
+	return res
+}
+
+// UpsertNewAuthKey inserts a new auth key into the database if not existed otherwise re-enable it.
+func UpsertNewAuthKey(authKey string) {
+	conn := GetConn()
+	defer conn.Close(context.Background())
+
+	authKey = strings.TrimSpace(authKey)
+	var isDisabled bool
+	err := conn.QueryRow(context.Background(), "select auth_key, is_disabled from auth_keys where auth_key=$1", authKey).Scan(&authKey, &isDisabled)
+	if err != nil {
+		log.Error("query database failed", "err", err)
+	} else {
+		if isDisabled {
+			_, err := conn.Exec(context.Background(), "update auth_keys set is_disabled=false where auth_key=$1", authKey)
+			if err != nil {
+				log.Error("update database failed", "err", err)
+			}
+		}
+		return
+	}
+
+	_, err = conn.Exec(context.Background(), "insert into auth_keys (auth_key) values($1)", authKey)
+	if err != nil {
+		log.Error("insert database failed", "err", err)
+		return
+	}
 }
--- auth_test.go
+++ auth_test.go
@@ -4,9 +4,12 @@ import "testing"
 
 func TestAuthStore(t *testing.T) {
 	a := NewAuthStore()
+	// test Add key
 	a.Add("key", "value")
+
+	// test Valid key returns value
 	isValid := a.Valid("key", "value")
-	if !isValid {
+	if isValid == "" {
 		t.Errorf("Expected true, got %v", isValid)
 	}
 
@@ -14,7 +17,14 @@ func TestAuthStore(t *testing.T) {
 	a.Remove("key")
 
 	isValid = a.Valid("key", "value")
-	if isValid {
-		t.Errorf("Expected false, got %v", isValid)
+	if isValid != "" {
+		t.Errorf("Expected empty string, got %v", isValid)
+	}
+
+	// test Get
+	a.Add("key", "value")
+	val := a.Get("key")
+	if val != "value" {
+		t.Errorf("Expected value, got %v", val)
 	}
 }
